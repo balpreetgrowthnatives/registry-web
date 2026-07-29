4c94a797131518911694580c122a95176f6564ac
Balpreet Brar <balpreet.brar@growthnatives.com>
2026-07-29 16:07:13 +0530

    Refactor CanonicalLink into reusable component as suggested by CodeRabbit

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,20 +1,11 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router'
 import React from 'react'
 import SignIn from '../../components/AuthUI/AuthUI'
-import { SITE_URL } from '../../src/constants'
+import { CanonicalLink } from '../../components/CanonicalLink'
 
 const SignInPage: React.FC = () => {
-  const router = useRouter()
-  const localePrefix =
-    router.locale === router.defaultLocale ? '' : `/${router.locale}`
-  const canonicalUrl = `${SITE_URL}${localePrefix}${router.pathname}`
-
   return (
     <>
-      <Head>
-        <link rel="canonical" href={canonicalUrl} />
-      </Head>
+      <CanonicalLink />
       <SignIn />
     </>
   )
--- pages/auth/logout.tsx
+++ pages/auth/logout.tsx
@@ -1,20 +1,11 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router'
 import React from 'react'
 import Logout from '../../components/AuthUI/Logout'
-import { SITE_URL } from '../../src/constants'
+import { CanonicalLink } from '../../components/CanonicalLink'
 
 const LogoutPage: React.FC = () => {
-  const router = useRouter()
-  const localePrefix =
-    router.locale === router.defaultLocale ? '' : `/${router.locale}`
-  const canonicalUrl = `${SITE_URL}${localePrefix}${router.pathname}`
-
   return (
     <>
-      <Head>
-        <link rel="canonical" href={canonicalUrl} />
-      </Head>
+      <CanonicalLink />
       <Logout />
     </>
   )
